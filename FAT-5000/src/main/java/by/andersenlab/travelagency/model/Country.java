2252e6fa44d759725c69ca05d658f828061d70ca
Sergey Borsuk <s.borsuk@andersenlab.com>
2019-11-11 14:44:10 +0300

    Create model for travel agency

---
+++ FAT-5000/src/main/java/by/andersenlab/travelagency/model/Country.java
@@ -0,0 +1,13 @@
+package by.andersenlab.travelagency.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Country {
+    private Integer id;
+    private String nameCountry;
+}
