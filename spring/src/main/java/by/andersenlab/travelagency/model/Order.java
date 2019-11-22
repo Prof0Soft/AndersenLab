15033f6a2789bb0dd272d949d4bd84d309e59439
Sergey Borsuk <s.borsuk@andersenlab.com>
2019-11-22 14:23:36 +0300

    add spring project

---
+++ spring/src/main/java/by/andersenlab/travelagency/model/Order.java
@@ -0,0 +1,33 @@
+package by.andersenlab.travelagency.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "USERORDER")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "NAMEORDER")
+    private String nameOrder;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne
+    @JoinColumn(name = "IDUSER")
+    private User userId;
+
+    @Column(name = "PRICE")
+    private Double price;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Set<Tour> tours = new HashSet<>();
+}
